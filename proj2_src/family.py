575ad26a3abeebc31612fe3fc7093c91952f6a5a
Glenn Olsson <glennholsson@gmail.com>
2021-12-03 11:38:12 -0800

    Make better family tree structure

--- proj2_src/family.py
+++ proj2_src/family.py
@@ -1,59 +1,150 @@
 from __future__ import annotations
-from typing import Optional, Set, List
+from typing import Optional, Set, List, cast
 
 from uuid import UUID, uuid4
 from proj2_src.sex import approximate_sex, Sex
+
+def childs(p1: Optional[Person], p2: Optional[Person]) -> Set[Person]:
+	"""Returns children has between two people"""
+	# Handle cases where not all parents are known
+	if p1 is None: # If p1 is none, we cannot have set p2 either
+		return set()
+	if p2 is None: # p1 must be non-None here
+		return p1.children
+	
+	# Else, knows both parents
+
+	p1_c = p1.children
+	p2_c = p2.children
+
+	return p1_c.intersection(p2_c)
 
 class Person:
 	name: str
 	sex: Sex
 	_id: UUID
 
-	child_family: Optional[Family]
-	parent_families: Set[Family] # Can be parent in multiple families, but only in one family once
+	parent1: Optional[Person]
+	parent2: Optional[Person]
+
+	children: Set[Person]
 
 	def __init__(self, name: str):
 		self.name = name
 		self._id = uuid4()
 		self.sex = approximate_sex(name)
+		
+		self.parent1 = None
+		self.parent2 = None
 
-		self.child_family = None
-		self.parent_families = set()
+		self.children = set()
 	
+	def add_child(self, person: Person):
+		# If already in. To handle recursive calls
+		if person in self.children:
+			return
+		self.children.add(person)
+		person.add_parent(self)
+	
+	def add_parent(self, p: Person):
+		# If person already is set to one of the parents, skip doing anything
+		if p == self.parent1 or p == self.parent2:
+			return
+		if self.parent1 is None:
+			self.parent1 = p
+		else: # Assumes that parent2 is None. If adding more than 2 parents, parent2 will always be replaced
+			self.parent2 = p
+
+		p.add_child(self)
+
+	def families(self) -> Set[Family]:
+		families: Set[Family] = set()
+
+		siblings = childs(self.parent1, self.parent2) # Siblings including person self
+		child_family = Family(self.parent1, self.parent2, siblings)
+		families.add(child_family)
+
+		# Create all families where self is a parent
+		for c in self.children:
+			c_p1: Optional[Person] = c.parent1
+			c_p2: Optional[Person] = c.parent2
+
+			siblings = childs(c_p1, c_p2) # Siblings including c
+
+			c_fam = Family(c_p1, c_p2, siblings)
+			families.add(c_fam)
+		
+		return families
+	
+	def __hash__(self) -> int:
+		return hash(self._id)
+
 	def __str__(self):
 		return f"Person({self.name})"
 
 class Family:
+	"""Assumes that family (that can have children) are between a male and a female which is only relevant for
+	GEDCOM output"""
 
-	spouse1: Optional[Person]
-	spouse2: Optional[Person]
+	husband: Optional[Person]
+	wife: Optional[Person]
 
 	children: Set[Person]
 
-	_id: UUID
+	def __init__(self, s1: Optional[Person], s2: Optional[Person], children: Set[Person]):
+		self.assign_parents(s1, s2)
 
-	def __init__(self):
-		self.spouse1 = None
-		self.spouse2 = None
-
-		self.children = set()
-
-		self._id = uuid4()
+		self.children = children
 	
-	def add_child(self, person: Person):
-		"""Adds person as child and sets persons child_family as self"""
-		self.children.add(person)
-		person.child_family = self
-	
-	def add_spouse(self, person: Person):
-		person.parent_families.add(self)
-		if self.spouse1 is None:
-			self.spouse1 = person
-		else: # Assumes that spouse2 is None. If adding more than 2 spouses, spouse2 will always be replaced
-			self.spouse2 = person
+	def assign_parents(self, sp1: Optional[Person], sp2: Optional[Person]):
+		"""Assign a husband and wife on best guess"""
+		sp1_sex: Sex = sp1.sex if sp1 is not None else Sex.undefined
+		sp2_sex: Sex = sp2.sex if sp2 is not None else Sex.undefined
+
+		# For case where both people have same sex, one of them will be assigned other sex
+		if sp1_sex == Sex.undefined and sp2_sex == Sex.undefined:
+			# Just assign something, couldn't guess who was who
+			self.husband = sp1
+			self.wife = sp2
+		elif sp1_sex != Sex.undefined:
+			if sp1_sex == Sex.male:
+				self.husband = sp1
+				self.wife = sp2
+			else:
+				self.wife = sp1
+				self.husband = sp2
+		else: # sp2_sex != Sex.undefined
+			if sp2_sex == Sex.male:
+				self.wife = sp1
+				self.husband = sp2
+			else:
+				self.husband = sp1
+				self.wife = sp2
 		
-	def __str__(self):
-		return f"Family(sp1={self.spouse1}, sp2={self.spouse2}, children={self.children})"
+	def is_meaningful(self) -> bool:
+		"""Returns if there are at least two people in this family, i.e. is meaningful"""
+		i = 0
+		if self.husband is not None:
+			i += 1
+		if self.wife is not None:
+			i += 1
 
-	def gedcom(self):
-		return GEDCOMFamily(self)
+		i += len(self.children)
+
+		return i >= 2
+	
+	def __eq__(self, o):
+		if type(o) is not Family:
+			return false
+		
+		# Cast for mypy
+		o = cast(Family, o)
+		
+		return self.husband == o.husband and self.wife == o.wife
+
+	def __hash__(self) -> int:
+		"""A husband and wife will only have 1 family"""
+		return hash(self.husband) ^ hash(self.wife)
+	
+	def __str__(self) -> str:
+		return f"Family(husband={self.husband}, wife={self.wife}, children={self.children})"
--- tests/tree_test.py
+++ tests/tree_test.py
@@ -11,44 +11,54 @@ class TestTree(unittest.TestCase):
 
 	def setUp(self):
 
-		glenn_node = Person("Glenn Olsson")
+		glenn_person = Person("Glenn Olsson")
 
-		glenn_dad_node = Person("Jonny Olsson")
-		glenn_mom_node = Person("Christina Carlsson")
+		glenn_dad_person = Person("Jonny Olsson")
+		glenn_mom_person = Person("Christina Carlsson")
 
-		glenn_sis1_node = Person("Josefine Törnqvist")
-		glenn_sis2_node = Person("Camilla Olsson")
+		glenn_sis1_person = Person("Josefine Törnqvist")
+		glenn_sis2_person = Person("Camilla Olsson")
 
-		glenn_family = Family()
+		glenn_person.add_parent(glenn_dad_person)
+		glenn_person.add_parent(glenn_mom_person)
 
-		glenn_family.add_child(glenn_node)
+		glenn_dad_person.add_child(glenn_sis1_person)
+		glenn_dad_person.add_child(glenn_sis2_person)
 
-		glenn_family.add_child(glenn_sis1_node)
-		glenn_family.add_child(glenn_sis2_node)
+		glenn_mom_person.add_child(glenn_sis1_person)
+		glenn_mom_person.add_child(glenn_sis2_person)
 
-		glenn_family.add_spouse(glenn_dad_node)
-		glenn_family.add_spouse(glenn_mom_node)
+		self.glenn_tree = Tree(glenn_person)
+		self.glenn = glenn_person
+		self.all_glenn_tree_persons = [glenn_person, glenn_dad_person, glenn_mom_person, glenn_sis1_person, glenn_sis2_person]
+		assert(len(self.all_glenn_tree_persons) == 5)
 
-		self.glenn_tree = Tree(glenn_family)
-		self.glenn = glenn_node
-		self.all_glenn_tree_nodes = [glenn_node, glenn_dad_node, glenn_mom_node, glenn_sis1_node, glenn_sis2_node]
-		assert(len(self.all_glenn_tree_nodes) == 5)
+	def testPeopleIteratesThroughAllPersons(self):
 
-	# def testGlennHas2Siblings(self):
-	# 	glenn_full_siblings = self.glenn.full_siblings()
-	# 	# should be same in this case
-	# 	glenn_siblings = self.glenn.siblings()
+		visited = set() # Easiest comparision is with set vs set, instead of list vs list as order would matter
+		visited_count = 0 # To make sure that not to many items are added
+		for p in self.glenn_tree:
+			visited.add(p)
+			visited_count += 1
 
-	# 	self.assertSetEqual(glenn_full_siblings, glenn_siblings)
-	# 	self.assertEqual(len(glenn_full_siblings), 2)
-
-	def testPeopleIteratesThroughAllNodes(self):
-
-		visited = set()
-		for n in self.glenn_tree.people():
-			visited.add(n)
+		self.assertEqual(len(visited), visited_count)
 		
-		expected_visited = set(self.all_glenn_tree_nodes)
+		expected_visited = set(self.all_glenn_tree_persons)
 
 		self.assertEqual(len(visited), len(expected_visited))
 		self.assertSetEqual(visited, expected_visited)
+	
+	def testMeaningfulFamiliesIs1(self):
+		fams = set()
+		for p in self.glenn_tree:
+			families = p.families()
+			meaningful_fams = [f for f in families if f.is_meaningful()]
+			fams.update(meaningful_fams)
+
+		self.assertEqual(len(fams), 1)
+
+	def testGlennsDadHasNonMeaningfulFamilies(self):
+		# Will have non-meaningful family as parents are not defined
+		fams = self.all_glenn_tree_persons[1].families()
+		meaningful_fams = [f for f in fams if f.is_meaningful()]
+		self.assertGreater(len(fams), len(meaningful_fams))
